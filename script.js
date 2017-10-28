c621ace85777b6396ed76e852aa81ab8d95d4068
MustinJoss <31445480+MustinJoss@users.noreply.github.com>
2017-10-28 18:00:42 -0500

    Uploaded initial html, js, and css files.

---
+++ script.js
@@ -0,0 +1,39 @@
+var box = document.getElementById('display');
+
+function input(x) {
+    box.value += x;
+    if (x === 'c') {
+        box.value = '';
+    }
+
+    if (box.value === 'error' + x) {
+        box.value = x;
+    }
+
+    var num = box.value;
+}
+
+function equals() {
+    x = box.value;
+    x = eval(x);
+    box.value = x;
+
+    if (x === undefined) {
+        box.value = 'error'
+    }
+}
+
+function backspace() {
+    if (box.value === 'error') {
+       box.value = '';
+    } else {
+        var num = box.value;
+        var len = num.length - 1;
+        var newNum = num.substring(0,len);
+        box.value = newNum;
+
+        if (newNum.length < 1) {
+           box.value = '';
+        }
+    }
+}
